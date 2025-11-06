a2e61aec921e28678e3c40e293d43eaf5bf5cbeb
Hamza <hamzafareed616@gmail.com>
2025-11-06 19:56:49 +0500

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { FormComponent } from './form-component/form-component';
+import { ParentComponent } from './parent-component/parent-component';
+import { Dashboard } from './dashboard/dashboard';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+     { path: '', component:  Dashboard},
+  { path: 'form', component: FormComponent },
+  { path: 'parent', component: ParentComponent },
+ 
+];
